d45336e3960ef61beef3967ac5c0f0d4d8760679
Kevin Tourret <tourret.kevin@gmail.com>
2021-03-18 15:27:45 +0100

    Add region name to department show

--- src/app/department-show/department-show.component.ts
+++ src/app/department-show/department-show.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Department} from '../../models/department';
 import {DepartmentService} from '../../services/department.service';
 import {ActivatedRoute} from '@angular/router';
+import {Region} from '../../models/region';
+import {RegionService} from '../../services/region.service';
 
 @Component({
   selector: 'app-department-show',
@@ -11,15 +13,20 @@ import {ActivatedRoute} from '@angular/router';
 export class DepartmentShowComponent implements OnInit {
 
   department: Department;
+  region: Region;
 
   constructor(
     public departmentService: DepartmentService,
+    private regionService: RegionService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
       this.department = this.departmentService.getDepartment(params.code);
+      if (this.department) {
+        this.region = this.regionService.getRegion(this.department.codeRegion);
+      }
     });
   }
 
